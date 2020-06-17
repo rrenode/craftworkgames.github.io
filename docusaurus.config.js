163220df1bccb53162a2d5154675a029b1c4f3cb
Lucas Girouard-Stranks <lustranks@gmail.com>
2020-06-16 23:41:59 -0400

    Docusaurus setup

---
+++ docusaurus.config.js
@@ -0,0 +1,96 @@
+module.exports = {
+  title: 'MonoGame.Extended',
+  tagline: 'Extensions to make MonoGame more awesome',
+  url: 'http://www.monogameextended.net',
+  baseUrl: '/',
+  favicon: 'img/favicon.ico',
+  organizationName: 'craftworkgames', // Usually your GitHub org/user name.
+  projectName: 'website', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'MonoGame.Extended',
+      logo: {
+        alt: 'MonoGame.Extended',
+        src: 'img/logo.svg',
+      },
+      links: [
+        {
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: 'Docs',
+          position: 'left',
+        },
+        {to: 'blog', label: 'Blog', position: 'left'},
+        {
+          href: 'https://github.com/craftworkgames/MonoGame.Extended',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Learn',
+          items: [
+            {
+              label: 'Documentation',
+              to: 'docs/',
+            }
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'MonoGame Forums',
+              href: 'http://community.monogame.net/category/extended',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discord.gg/BVXNYUf',
+            }
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: 'blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/craftworkgames/MonoGame.Extended',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Craftwork Games`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          // It is recommended to set document id as docs home page (`docs/` path).
+          homePageId: 'about/introduction',
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/craftworkgames/website/tree/master',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl: null,
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
---
+++ sidebars.js
@@ -0,0 +1,6 @@
+module.exports = {
+  docs: {
+    'MonoGame.Extended': ['about/introduction', 'about/principles'],
+    'Getting Started': ['getting-started/installation', 'getting-started/contributing']
+  },
+};
